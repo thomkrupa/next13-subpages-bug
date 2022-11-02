38688a8485609d8e5cddc588fcaf81d375a82b3c
thom krupa <thom@bejamas.io>
2022-11-02 21:51:34 +0100

    add catch all

--- app/ssr/[...id]/page.tsx
+++ app/ssr/[...id]/page.tsx
@@ -1,6 +1,6 @@
 async function fetchData(params: { id: string }) {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${params.id}`,
+    `https://jsonplaceholder.typicode.com/posts/${params.id[0]}`,
   );
   const data = await res.json();
   return data;
@@ -14,6 +14,15 @@ export default async function Page({
 }) {
   const data = await fetchData(params);
 
+  if (params.id[1] === 'foo') {
+    return (
+      <div className="space-y-4">
+        <h2>foo!</h2>
+        <h1 className="text-2xl font-medium text-zinc-200">{data.title}</h1>
+        <p className="font-medium text-zinc-500">{data.body}</p>
+      </div>
+    );
+  }
   return (
     <div className="space-y-4">
       <h1 className="text-2xl font-medium text-zinc-200">{data.title}</h1>
