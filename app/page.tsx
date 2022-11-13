b5cfb3fb820051cc443cba2159106a3579d61b4b
thom krupa <thom@bejamas.io>
2022-11-13 17:26:08 +0100

    refactor pages

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,31 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <div>
-      <p>homepage</p>
-      <Link href="/ssr">SSR demo</Link>
+      <p>ISR demo</p>
+      <h3>nodejs runtime, ISR is working</h3>
+      <ul>
+        <li>
+          <Link href="/isr/1/node">Node page 1</Link>
+        </li>
+        <li>
+          <Link href="/isr/2/node">Node page 2</Link>
+        </li>
+        <li>
+          <Link href="/isr/3/node">Node page 3</Link>
+        </li>
+      </ul>
+      <h3>experimental-edge runtime, ISR not working</h3>
+      <ul>
+        <li>
+          <Link href="/isr/1/edge">Node page 1</Link>
+        </li>
+        <li>
+          <Link href="/isr/2/edge">Node page 2</Link>
+        </li>
+        <li>
+          <Link href="/isr/3/edge">Node page 3</Link>
+        </li>
+      </ul>
     </div>
   );
 }
